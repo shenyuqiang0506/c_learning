9dd048cb3daa12913421513524fa9b21237cb3e3
山河不入心 <2861476552@qq.com>
2025-08-07 15:58:29 +0800

    求10+20的和

---
+++ Function/02_function_demo2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//
+// Created by shen on 25-8-7.
+//
+void sum() {
+    int num1 = 10;
+    int num2 = 20;
+    int sum = num1 + num2;
+    printf("%d\n", sum);
+}
+
+int main() {
+    sum();
+    return 0;
+}
