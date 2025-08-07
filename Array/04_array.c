98ea82854927f19541355c45fd7fcab3d7ba5e21
山河不入心 <2861476552@qq.com>
2025-08-07 18:06:43 +0800

    数组的遍历

---
+++ Array/04_array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//
+// Created by shen on 25-8-7.
+//数组的遍历
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    for (int i = 0; i < 10; ++i) {
+        printf("arr[%d] = %d\n", i, arr[i]);
+    }
+}
