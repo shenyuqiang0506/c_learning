147e06037040c3a9bffea573bc4397439d4612aa
山河不入心 <2861476552@qq.com>
2025-08-07 15:48:45 +0800

    函数最基本的定义方式

---
+++ Function/01_function_demo1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+void playGame() {
+    printf("选择人物\n");
+    printf("选择人物\n");
+    printf("选择人物\n");
+    printf("选择人物\n");
+    printf("选择人物\n");
+    printf("选择人物\n");
+    printf("选择人物\n");
+    printf("选择人物\n");
+}
+
+int main() {
+    /*
+     函数最基本的定义方式
+    void 函数名() {
+        函数体；
+            }
+     */
+    playGame();
+    return 0;
+}
