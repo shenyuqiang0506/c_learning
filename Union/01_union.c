b7bc4376f729339d9e58a5ba3360507326bfebfb
山河不入心 <2861476552@qq.com>
2025-08-11 16:40:06 +0800

    联合体

---
+++ Union/01_union.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+union MoneyType {
+    int moneyi;
+    double moneyd;
+    char moneys[100];
+};
+
+int main() {
+    /*
+    *需求:
+    金融项目中，钱有可能是整数，小数，字符串，请定义对应的共同体
+     */
+    union MoneyType money;
+    //赋值
+    //每次只能赋值一个
+    money.moneyi = 1;
+}
